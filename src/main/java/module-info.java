4be3f8b132978632449ec7d4ecdcb4d3c4c3666d
Baptiste Fourmont <baptiste_fourmont@tutanota.com>
2021-12-18 19:02:02 +0100

    ADD: Save Image

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.fractaly {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.graphics;
     requires commons.cli;
     exports org.fractaly;
 }
--- src/main/java/org/fractaly/App.java
+++ src/main/java/org/fractaly/App.java
@@ -2,13 +2,25 @@ package org.fractaly;
 
 import javafx.fxml.*;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import org.fractaly.screens.Fractal;
 import org.fractaly.utils.Complex;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -21,11 +33,33 @@ import org.apache.commons.cli.Options;
  * JavaFX App
  */
 public class App extends Application {
-    @Override
-    public void start(Stage stage) {
-        Fractal f = Fractal.buildJulia(1000, 1000, 1000, Complex.build(-0.4, 0.6));
-        ImageView v = new ImageView(f);
 
+    private static void saveImageFile(WritableImage writableImage, Stage stage) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "image files (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showSaveDialog(stage);
+
+        if (file != null) {
+            String fileName = file.getName();
+
+            if (!fileName.toUpperCase().endsWith(".PNG")) {
+                file = new File(file.getAbsolutePath() + ".png");
+            }
+
+            ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null),"png", file);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Fractal f = Fractal.buildJulia(1000, 1000, 1000, Complex.build(-0.4, 0.6));
+        saveImageFile(f, stage);
+        ImageView v = new ImageView(f);
+   
         var scene = new Scene(new StackPane(v), 1000, 1000);
         stage.setScene(scene);
         stage.show();
